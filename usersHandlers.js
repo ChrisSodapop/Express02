fa0272440a7759e4d87601ca2d330745325a3504
ChrisSodapop <krisskross93700@gmail.com>
2022-08-23 13:13:39 +0200

    Exo 04 terminé

--- usersHandlers.js
+++ usersHandlers.js
@@ -7,20 +7,35 @@ const getUsers = (req, res) => {
 };
 
 const getUserById = (req, res) => {
-    const id = parseInt(req.params.id);
-  
-    database
-      .query("select * from users where id = ?", [id])
-      .then(([users]) => {
-        if (users[0] != null) {
-          res.status(200).json(users[0]);
-        } else {
-          res.status(404).send("Not Found");
-        }
-      })
-  };
+  const id = parseInt(req.params.id);
 
+  database.query("select * from users where id = ?", [id]).then(([users]) => {
+    if (users[0] != null) {
+      res.status(200).json(users[0]);
+    } else {
+      res.status(404).send("Not Found");
+    }
+  });
+};
+
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
 module.exports = {
   getUsers,
+  postUser,
   getUserById,
 };
