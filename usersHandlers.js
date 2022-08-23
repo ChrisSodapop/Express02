1386cb55be7be006338629eb09bbb6972a707370
ChrisSodapop <krisskross93700@gmail.com>
2022-08-23 11:21:56 +0200

    exo2 fini

---
+++ usersHandlers.js
@@ -0,0 +1,26 @@
+const database = require("./database");
+
+const getUsers = (req, res) => {
+  database.query("select * from users").then(([users]) => {
+    res.status(200).json(users);
+  });
+};
+
+const getUserById = (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    database
+      .query("select * from users where id = ?", [id])
+      .then(([users]) => {
+        if (users[0] != null) {
+          res.status(200).json(users[0]);
+        } else {
+          res.status(404).send("Not Found");
+        }
+      })
+  };
+
+module.exports = {
+  getUsers,
+  getUserById,
+};
