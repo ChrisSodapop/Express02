ddd8f010902b042c10cbab3bbc3ee3f6abf4b2c4
ChrisSodapop <krisskross93700@gmail.com>
2022-09-12 11:02:30 +0200

    Exo 07 fini

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
+const { hashPassword } = require("./auth.js");
+
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
@@ -22,13 +24,12 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUserById);
-app.post("/api/users", validateUser, usersHandlers.postUser);
+app.post("/api/users", validateUser, hashPassword, usersHandlers.postUser);
 app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
-app.put("/api/users/:id", validateUser,usersHandlers.updateUser);
+app.put("/api/users/:id", validateUser, usersHandlers.updateUser);
 app.post("/api/movies", validateMovie, movieHandlers.postMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie)
-app.delete("/api/users/:id", usersHandlers.deleteUser)
-
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", usersHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
